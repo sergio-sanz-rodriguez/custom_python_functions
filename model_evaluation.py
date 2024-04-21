37f5c339d27fac373b778624e14f8cf1d32fc3fa
Sergio <sergio.sanz.rodriguez@gmail.com>
2024-04-21 09:59:30 +0200

    added plotting loss function that is updated per epoch, very usefull for traning NNs

--- model_evaluation.py
+++ model_evaluation.py
@@ -437,4 +437,45 @@ def find_roc_threshold_accuracy(model, X, y):
             best_acc_core = cost
             best_threshold = i/10
       
-    return best_threshold, best_acc_score 
+    return best_threshold, best_acc_score
+
+
+class plot_learning(keras.callbacks.Callback):
+    """
+    Callback to plot the learning curves of the model during training.
+    https://medium.com/geekculture/how-to-plot-model-loss-while-training-in-tensorflow-9fa1a1875a5
+    """
+    def on_train_begin(self, logs={}):
+        self.metrics = {}
+        for metric in logs:
+            self.metrics[metric] = []
+            
+
+    def on_epoch_end(self, epoch, logs={}):
+        # Storing metrics
+        for metric in logs:
+            if metric in self.metrics:
+                self.metrics[metric].append(logs.get(metric))
+            else:
+                self.metrics[metric] = [logs.get(metric)]
+        
+        # Plotting
+        metrics = [x for x in logs if 'val' not in x]
+
+        f, axs = plt.subplots(1, len(metrics), figsize=(15,5))
+        clear_output(wait=True)
+
+        for i, metric in enumerate(metrics):
+            axs[i].plot(range(1, epoch + 2), 
+                        self.metrics[metric], 
+                        label=metric)
+            if logs['val_' + metric]:
+                axs[i].plot(range(1, epoch + 2), 
+                            self.metrics['val_' + metric], 
+                            label='val_' + metric)
+                
+            axs[i].legend()
+            axs[i].grid()
+
+        plt.tight_layout()
+        plt.show()
