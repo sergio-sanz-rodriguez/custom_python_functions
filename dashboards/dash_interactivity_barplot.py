a5a4b46769b9ad85107f31fdba359182b328752f
Sergio <sergio.sanz.rodriguez@gmail.com>
2024-06-06 20:17:15 +0200

    added dash notebooks

---
+++ dashboards/dash_interactivity_barplot.py
@@ -0,0 +1,52 @@
+import pandas as pd
+import plotly.graph_objects as go
+import plotly.express as px
+import dash
+from dash import dcc
+from dash import html
+from dash.dependencies import Input, Output
+
+# Now we will read the dataset using the pd.read_csv() function.
+airline_data =  pd.read_csv('https://cf-courses-data.s3.us.cloud-object-storage.appdomain.cloud/IBMDeveloperSkillsNetwork-DV0101EN-SkillsNetwork/Data%20Files/airline_data.csv', 
+                            encoding = "ISO-8859-1",
+                            dtype={'Div1Airport': str, 'Div1TailNum': str, 
+                                   'Div2Airport': str, 'Div2TailNum': str})
+
+# The above code reads a CSV file called airline_data.csv from a URL using pandas, a popular data analysis library in Python.
+# The file is encoded using ISO-8859-1 character encoding, which is a standard way of representing characters in the file.
+# We defined data type of specific columns such as (Div1Airport, Div1TailNum, Div2Airport, and Div2TailNum) to be strings, which ensures that these columns are read as text instead of numbers.
+# The resulting data is stored in a pandas dataframe object called airline_data, which can be used for further analysis.
+
+# Create a dash application
+app = dash.Dash(__name__)
+
+# Create a dash application
+app.layout = html.Div(children=[ html.H1('Total number of flights to the destination state split by reporting airline',
+                            style={'textAlign': 'center', 'color': '#503D36', 'font-size': 40}),
+                            html.Div(["Input Year: ", dcc. Input(id='input-year',value='2010',
+                            type='number', style={'height':'50px', 'font-size': 35}),], 
+                            style={'font-size': 40}),html.Br(), html.Br(),
+                            html.Div(dcc.Graph(id='bar-plot')),]) 
+
+
+@app.callback( Output(component_id='bar-plot', component_property='figure'),
+               Input(component_id='input-year', component_property='value'))
+
+# Add computation to callback function and return graph
+def get_graph(entered_year):
+    # Select 2019 data
+    df =  airline_data[airline_data['Year']==int(entered_year)]
+    
+    # Group the data by Month and compute average over arrival delay time.
+    bar_data = df.groupby('DestState')['Flights'].sum().reset_index()
+    fig = px.bar(bar_data, x='DestState', y='Flights', title='Total number of flights to the destination state split by reporting airline')
+    fig.update_layout(title='Flights to Destination State', xaxis_title='DestState', yaxis_title='Flights')
+    return fig
+
+# Run the app
+if __name__ == '__main__':
+    app.run_server()
+
+    
+
+
