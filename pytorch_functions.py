40739d54317964f4dcd7a2882f964013cdd2d8b0
Sergio <sergio.sanz.rodriguez@gmail.com>
2024-10-14 14:26:04 +0200

    updated pytorch_functions.py

--- pytorch_functions.py
+++ pytorch_functions.py
@@ -112,7 +112,7 @@ def train_nn(model: torch.nn.Module,
     model.to(device)
     model.train() # put model in train mode
 
-    for epoch in tqdm(range(epochs)):
+    for epoch in (range(epochs)):
         
         ### Training
 
