91276067dad3e80e1d3cc911839d46ee589e5855
Sergio <sergio.sanz.rodriguez@gmail.com>
2024-10-14 13:59:05 +0200

    updated pytorch_functions.py

--- pytorch_functions.py
+++ pytorch_functions.py
@@ -107,6 +107,98 @@ def train_nn(model: torch.nn.Module,
     """train_step and test_step in one single function.
     """
 
+    def train_step(model: torch.nn.Module,
+               data_loader: torch.utils.data.DataLoader,
+               loss_fn: torch.nn.Module,
+               optimizer: torch.optim.Optimizer,
+               accuracy_fn,
+               device="cpu"):
+    
+        """Performs the training step of a neural network.
+
+        Args:
+            model (torch.nn.Module): A PyTorch model capable of making predictions on data_loader.
+            data_loader (torch.utils.data.DataLoader): The target dataset to predict on.
+            loss_fn (torch.nn.Module): The loss function of model.
+            accuracy_fn: An accuracy function to compare the models predictions to the truth labels.
+            device (str, optional): Target device to compute on. Defaults to device.
+
+        Returns:
+            Sumary of train loss and train accuracy.
+        """
+
+        train_loss, train_acc = 0, 0
+        model.to(device)
+        model.train() # put model in train mode
+        for batch, (X, y) in enumerate(data_loader):
+            # Send data to GPU
+            X, y = X.to(device), y.to(device)
+
+            # 1. Forward pass
+            y_pred = model(X)
+
+            # 2. Calculate loss
+            loss = loss_fn(y_pred, y)
+            train_loss += loss
+            train_acc += accuracy_fn(y_true=y,
+                                    y_pred=y_pred.argmax(dim=1)) # Go from logits -> pred labels
+
+            # 3. Optimizer zero grad
+            optimizer.zero_grad()
+
+            # 4. Loss backward
+            loss.backward()
+
+            # 5. Optimizer step
+            optimizer.step()
+
+        # Calculate loss and accuracy per epoch and print out what's happening
+        train_loss /= len(data_loader)
+        train_acc /= len(data_loader)
+        print(f"Train loss: {train_loss:.5f} | Train accuracy: {train_acc:.2f}%")
+
+    def test_step(model: torch.nn.Module,
+              data_loader: torch.utils.data.DataLoader,              
+              loss_fn: torch.nn.Module,
+              accuracy_fn,
+              device="cpu"):
+    
+        """Performs the test step of a neural network.
+
+        Args:
+            model (torch.nn.Module): A PyTorch model capable of making predictions on data_loader.
+            data_loader (torch.utils.data.DataLoader): The target dataset to predict on.
+            loss_fn (torch.nn.Module): The loss function of model.
+            accuracy_fn: An accuracy function to compare the models predictions to the truth labels.
+            device (str, optional): Target device to compute on. Defaults to device.
+
+        Returns:
+            Sumary of train loss and train accuracy.
+        """
+
+        test_loss, test_acc = 0, 0
+        model.to(device)
+        model.eval() # put model in eval mode
+        # Turn on inference context manager
+        with torch.inference_mode(): 
+            for X, y in data_loader:
+                # Send data to GPU
+                X, y = X.to(device), y.to(device)
+                
+                # 1. Forward pass
+                test_pred = model(X)
+                
+                # 2. Calculate loss and accuracy
+                test_loss += loss_fn(test_pred, y)
+                test_acc += accuracy_fn(y_true=y,
+                    y_pred=test_pred.argmax(dim=1) # Go from logits -> pred labels
+                )
+            
+            # Adjust metrics and print out
+            test_loss /= len(data_loader)
+            test_acc /= len(data_loader)
+            print(f"Test loss: {test_loss:.5f} | Test accuracy: {test_acc:.2f}%\n")
+
     for epoch in tqdm(range(epochs)):
         print(f"Epoch: {epoch}\n---------")
         train_step(model=model,
