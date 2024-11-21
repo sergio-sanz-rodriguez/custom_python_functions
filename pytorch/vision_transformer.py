680b907d1e7a95569d8ac5bb7f8ecf3b0e063725
Sergio <sergio.sanz.rodriguez@gmail.com>
2024-11-21 19:13:28 +0100

    added multilayer percepton block of the transformer

--- pytorch/vision_transformer.py
+++ pytorch/vision_transformer.py
@@ -91,3 +91,33 @@ class MultiheadSelfAttentionBlock(nn.Module):
                                              value=x, # value embeddings
                                              need_weights=False) # do we need the weights or just the layer outputs?
         return attn_output
+    
+
+class MLPBlock(nn.Module):
+    """Creates a layer normalized multilayer perceptron block ("MLP block" for short)."""
+    # Initialize the class with hyperparameters from Table 1 and Table 3
+    def __init__(self,
+                 embedding_dim:int=768, # Hidden Size D from Table 1 for ViT-Base
+                 mlp_size:int=3072, # MLP size from Table 1 for ViT-Base (X4)
+                 dropout:float=0.1): # Dropout from Table 3 for ViT-Base
+        super().__init__()
+
+        # Create the Norm layer (LN)
+        self.layer_norm = nn.LayerNorm(normalized_shape=embedding_dim)
+
+        # Create the Multilayer perceptron (MLP) layer(s)
+        self.mlp = nn.Sequential(
+            nn.Linear(in_features=embedding_dim,
+                      out_features=mlp_size),
+            nn.GELU(), # "The MLP contains two layers with a GELU non-linearity (section 3.1)."
+            nn.Dropout(p=dropout),
+            nn.Linear(in_features=mlp_size, # needs to take same in_features as out_features of layer above
+                      out_features=embedding_dim), # take back to embedding_dim
+            nn.Dropout(p=dropout) # "Dropout, when used, is applied after every dense layer.."
+        )
+
+    # Create a forward() method to pass the data throguh the layers
+    def forward(self, x):
+        x = self.layer_norm(x)
+        x = self.mlp(x)
+        return x
