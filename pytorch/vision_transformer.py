dc829cf2358e2f1db18393b79055a9c455e9c44f
Sergio <sergio.sanz.rodriguez@gmail.com>
2024-11-21 19:07:25 +0100

    added multihead self-attention class

--- pytorch/vision_transformer.py
+++ pytorch/vision_transformer.py
@@ -64,5 +64,30 @@ class PatchEmbedding(nn.Module):
 
 
 
+class MultiheadSelfAttentionBlock(nn.Module):
+    """Creates a multi-head self-attention block ("MSA block" for short).
+    """
+    # Initialize the class with hyperparameters from Table 1
+    def __init__(self,
+                 embedding_dim:int=768, # Hidden size D from Table 1 for ViT-Base
+                 num_heads:int=12, # Heads from Table 1 for ViT-Base
+                 attn_dropout:float=0): # doesn't look like the paper uses any dropout in MSABlocks
+        super().__init__()
 
-    
+        # Create the Norm layer (LN)
+        self.layer_norm = nn.LayerNorm(normalized_shape=embedding_dim)
+
+        # Create the Multi-Head Attention (MSA) layer
+        self.multihead_attn = nn.MultiheadAttention(embed_dim=embedding_dim,
+                                                    num_heads=num_heads,
+                                                    dropout=attn_dropout,
+                                                    batch_first=True) # does our batch dimension come first?
+
+    # Create a forward() method to pass the data throguh the layers
+    def forward(self, x):
+        x = self.layer_norm(x)
+        attn_output, _ = self.multihead_attn(query=x, # query embeddings
+                                             key=x, # key embeddings
+                                             value=x, # value embeddings
+                                             need_weights=False) # do we need the weights or just the layer outputs?
+        return attn_output
