942b1ad2c4d5a0ffbcf7f390da33b75fcbb3ae6f
Sergio <sergio.sanz.rodriguez@gmail.com>
2024-11-21 20:48:03 +0100

    added transformer encoder block class including multihead self-attention and multilayer perceptron network

--- pytorch/vision_transformer.py
+++ pytorch/vision_transformer.py
@@ -109,15 +109,48 @@ class MLPBlock(nn.Module):
         self.mlp = nn.Sequential(
             nn.Linear(in_features=embedding_dim,
                       out_features=mlp_size),
-            nn.GELU(), # "The MLP contains two layers with a GELU non-linearity (section 3.1)."
+            nn.GELU(), # "The MLP contains two layers with a GELU non-linearity"
             nn.Dropout(p=dropout),
             nn.Linear(in_features=mlp_size, # needs to take same in_features as out_features of layer above
                       out_features=embedding_dim), # take back to embedding_dim
-            nn.Dropout(p=dropout) # "Dropout, when used, is applied after every dense layer.."
+            nn.Dropout(p=dropout) # "Dropout, when used, is applied after every dense layer"
         )
 
     # Create a forward() method to pass the data throguh the layers
     def forward(self, x):
         x = self.layer_norm(x)
         x = self.mlp(x)
+        return x
+    
+
+class TransformerEncoderBlock(nn.Module):
+    """Creates a Transformer Encoder block."""
+    # Initialize the class with hyperparameters from Table 1 and Table 3
+    def __init__(self,
+                 embedding_dim:int=768, # Hidden size D from Table 1 for ViT-Base
+                 num_heads:int=12, # Heads from Table 1 for ViT-Base
+                 mlp_size:int=3072, # MLP size from Table 1 for ViT-Base
+                 mlp_dropout:float=0.1, # Amount of dropout for dense layers from Table 3 for ViT-Base
+                 attn_dropout:float=0): # Amount of dropout for attention layers
+        super().__init__()
+
+        # Create MSA block (equation 2)
+        self.msa_block = MultiheadSelfAttentionBlock(embedding_dim=embedding_dim,
+                                                     num_heads=num_heads,
+                                                     attn_dropout=attn_dropout)
+
+        # Create MLP block (equation 3)
+        self.mlp_block =  MLPBlock(embedding_dim=embedding_dim,
+                                   mlp_size=mlp_size,
+                                   dropout=mlp_dropout)
+
+    # Create a forward() method
+    def forward(self, x):
+
+        # Create residual connection for MSA block (add the input to the output)
+        x =  self.msa_block(x) + x
+
+        # Create residual connection for MLP block (add the input to the output)
+        x = self.mlp_block(x) + x
+
         return x
