9db56c06ec4109f7154f08c66b5ae5b982d9a435
Sergio <sergio.sanz.rodriguez@gmail.com>
2024-11-22 14:11:14 +0100

    implemented ViT class with all the pieces together

--- pytorch/vision_transformer.py
+++ pytorch/vision_transformer.py
@@ -2,6 +2,7 @@ import torch
 import torchvision
 from torch import nn
 
+# Implementation of a vision transformer following the paper "AN IMAGE IS WORTH 16X16 WORDS: TRANSFORMERS FOR IMAGE RECOGNITION AT SCALE"
 class PatchEmbedding(nn.Module):
     """Turns a 2D input image into a 1D sequence learnable embedding vector.
 
@@ -118,9 +119,7 @@ class MLPBlock(nn.Module):
 
     # Create a forward() method to pass the data throguh the layers
     def forward(self, x):
-        x = self.layer_norm(x)
-        x = self.mlp(x)
-        return x
+        return self.mlp(self.layer_norm(x))
     
 
 class TransformerEncoderBlock(nn.Module):
@@ -152,5 +151,90 @@ class TransformerEncoderBlock(nn.Module):
 
         # Create residual connection for MLP block (add the input to the output)
         x = self.mlp_block(x) + x
+
+        return x
+    
+
+# 1. Create a ViT class that inherits from nn.Module
+class ViT(nn.Module):
+    """Creates a Vision Transformer architecture with ViT-Base hyperparameters by default."""
+    # 2. Initialize the class with hyperparameters from Table 1 and Table 3
+    def __init__(self,
+                 img_size:int=224, # Training resolution from Table 3 in ViT paper
+                 in_channels:int=3, # Number of channels in input image
+                 patch_size:int=16, # Patch size
+                 num_transformer_layers:int=12, # Layers from Table 1 for ViT-Base
+                 embedding_dim:int=768, # Hidden size D from Table 1 for ViT-Base
+                 mlp_size:int=3072, # MLP size from Table 1 for ViT-Base
+                 num_heads:int=12, # Heads from Table 1 for ViT-Base
+                 attn_dropout:float=0, # Dropout for attention projection
+                 mlp_dropout:float=0.1, # Dropout for dense/MLP layers
+                 embedding_dropout:float=0.1, # Dropout for patch and position embeddings
+                 num_classes:int=1000): # Default for ImageNet but can customize this
+        super().__init__() # don't forget the super().__init__()!
+
+        # 3. Make the image size is divisble by the patch size
+        assert img_size % patch_size == 0, f"Image size must be divisible by patch size, image size: {img_size}, patch size: {patch_size}."
+
+        # 4. Calculate number of patches (height * width/patch^2)
+        self.num_patches = (img_size * img_size) // patch_size**2
+
+        # 5. Create learnable class embedding (needs to go at front of sequence of patch embeddings)
+        self.class_embedding = nn.Parameter(data=torch.randn(1, 1, embedding_dim),
+                                            requires_grad=True)
+
+        # 6. Create learnable position embedding
+        self.position_embedding = nn.Parameter(data=torch.randn(1, self.num_patches+1, embedding_dim),
+                                               requires_grad=True)
+
+        # 7. Create embedding dropout value
+        self.embedding_dropout = nn.Dropout(p=embedding_dropout)
+
+        # 8. Create patch embedding layer
+        self.patch_embedding = PatchEmbedding(in_channels=in_channels,
+                                              patch_size=patch_size,
+                                              embedding_dim=embedding_dim)
+
+        # 9. Create Transformer Encoder blocks (we can stack Transformer Encoder blocks using nn.Sequential())
+        # Note: The "*" means "all"
+        self.transformer_encoder = nn.Sequential(*[TransformerEncoderBlock(embedding_dim=embedding_dim,
+                                                                           num_heads=num_heads,
+                                                                           mlp_size=mlp_size,
+                                                                           mlp_dropout=mlp_dropout,
+                                                                           attn_dropout=attn_dropout) for _ in range(num_transformer_layers)])
+
+        # 10. Create classifier head
+        self.classifier = nn.Sequential(
+            nn.LayerNorm(normalized_shape=embedding_dim),
+            nn.Linear(in_features=embedding_dim,
+                      out_features=num_classes)
+        )
+
+    # 11. Create a forward() method
+    def forward(self, x):
+
+        # 12. Get batch size
+        batch_size = x.shape[0]
+
+        # 13. Create class token embedding and expand it to match the batch size (equation 1)
+        class_token = self.class_embedding.expand(batch_size, -1, -1) # "-1" means to infer the dimension (try this line on its own)
+
+        # 14. Create patch embedding (equation 1)
+        x = self.patch_embedding(x)
+
+        # 15. Concat class embedding and patch embedding (equation 1)
+        x = torch.cat((class_token, x), dim=1)
+
+        # 16. Add position embedding to patch embedding (equation 1)
+        x = self.position_embedding + x
+
+        # 17. Run embedding dropout (Appendix B.1)
+        x = self.embedding_dropout(x)
+
+        # 18. Pass patch, position and class embedding through transformer encoder layers (equations 2 & 3)
+        x = self.transformer_encoder(x)
+
+        # 19. Put 0 index logit through classifier (equation 4)
+        x = self.classifier(x[:, 0]) # run on each sample in a batch at 0 index
 
         return x
